294464d120016f9b40b500a47669efb14b954320
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-07 14:00:17 -0300

    action-api-testing plugin has been created.

---
+++ action-api-testing/src/main/java/org/kayteam/actionapitesting/ActionApiTesting.java
@@ -0,0 +1,57 @@
+package org.kayteam.actionapitesting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.kayteam.actionapi.ActionManager;
+import org.kayteam.actionapitesting.commands.ActionApiTestingCommand;
+
+import java.io.File;
+
+public final class ActionApiTesting extends JavaPlugin {
+
+    ActionManager actionManager = new ActionManager( this );
+
+    @Override
+    public void onEnable() {
+
+        registerFiles();
+
+        registerManagers();
+
+        registerCommands();
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+
+    public ActionManager getActionManager() {
+        return actionManager;
+    }
+
+    private void registerFiles() {
+
+        File file = new File( getDataFolder() , "config.yml" );
+
+        if ( ! file.exists() ) {
+
+            saveResource( "config.yml" , true );
+
+        }
+
+    }
+
+    private void registerManagers() {
+        actionManager.registerManager();
+    }
+
+    private void registerCommands() {
+
+        getCommand( "ActionApiTesting" ).setExecutor( new ActionApiTestingCommand( this ) );
+
+        getCommand( "ActionApiTesting" ).setTabCompleter( new ActionApiTestingCommand( this ) );
+
+    }
+
+}
---
+++ action-api-testing/src/main/java/org/kayteam/actionapitesting/commands/ActionApiTestingCommand.java
@@ -0,0 +1,115 @@
+package org.kayteam.actionapitesting.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.kayteam.actionapi.Action;
+import org.kayteam.actionapi.ActionManager;
+import org.kayteam.actionapitesting.ActionApiTesting;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ActionApiTestingCommand implements CommandExecutor , TabCompleter {
+
+    private final ActionApiTesting actionApiTesting;
+
+    public ActionApiTestingCommand(ActionApiTesting actionApiTesting) {
+
+        this.actionApiTesting = actionApiTesting;
+
+    }
+
+    @Override
+    public boolean onCommand( CommandSender sender , Command command , String label , String[] args ) {
+
+        FileConfiguration config = actionApiTesting.getConfig();
+
+        if ( ! ( sender instanceof Player ) ) {
+
+            sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.onlyPlayers" ) ) );
+
+            return true;
+
+        }
+
+        Player player = ( Player ) sender;
+
+        if ( args.length < 1 ) {
+
+            sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.emptyArgs" ) ) );
+
+            return true;
+
+        }
+
+        String subcommand = args[0];
+
+        if ( subcommand.equalsIgnoreCase( "reload" ) ) {
+
+            sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.reloaded" ) ) );
+
+        } else if ( subcommand.equalsIgnoreCase( "test" ) ) {
+
+            if ( args.length < 2 ) {
+
+                sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.emptyAction" ) ) );
+
+                return true;
+
+            }
+
+            String actionType = args[1];
+
+            ActionManager actionManager = actionApiTesting.getActionManager();
+
+            if ( ! config.contains( "types." + actionType ) || ! actionManager.existActionExpansion( actionType ) ) {
+
+                sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.invalidAction" ) ) );
+
+                return true;
+
+            }
+
+            String actionFormat = config.getString( "types." + actionType );
+
+            Action action = actionManager.loadAction( actionFormat );
+
+            action.execute( player );
+
+            sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.executed" ) ) );
+
+        } else {
+
+            sender.sendMessage( ChatColor.translateAlternateColorCodes( '&' , config.getString( "messages.invalidArgs" ) ) );
+
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public List<String> onTabComplete( CommandSender sender , Command command , String alias , String[] args ) {
+
+        if ( args.length == 1 ) {
+
+            return Arrays.asList( "reload" , "test" );
+
+        }
+
+        if ( args.length == 2 ) {
+
+            return Arrays.asList( "actionbar" , "console" , "message" , "player" , "sound" );
+
+        }
+
+        return null;
+
+    }
+
+}
