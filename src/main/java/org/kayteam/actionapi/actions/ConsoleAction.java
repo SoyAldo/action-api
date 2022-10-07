767a621fa52de6f247aad29272783cb1e99057d9
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 22:38:13 -0300

    ConsoleAction implemented.

---
+++ src/main/java/org/kayteam/actionapi/actions/ConsoleAction.java
@@ -0,0 +1,39 @@
+package org.kayteam.actionapi.actions;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.kayteam.actionapi.Action;
+import org.kayteam.actionapi.ActionManager;
+import org.kayteam.actionapi.util.PlaceholderAPIUtil;
+
+public class ConsoleAction extends Action {
+
+    public ConsoleAction( String value ) {
+        super( "console" , value );
+    }
+
+    @Override
+    public void execute( Player player ) {
+
+        String realValue = getValue();
+
+        realValue = PlaceholderAPIUtil.setPlaceholders( player , realValue );
+
+        realValue = ChatColor.translateAlternateColorCodes( '&' , realValue );
+
+        ActionManager actionManager = getActionManager();
+
+        JavaPlugin javaPlugin = actionManager.getJavaPlugin();
+
+        Server server = javaPlugin.getServer();
+
+        ConsoleCommandSender consoleCommandSender = server.getConsoleSender();
+
+        server.dispatchCommand( consoleCommandSender , realValue );
+
+    }
+
+}
---
+++ src/main/java/org/kayteam/actionapi/expansions/ConsoleExpansion.java
@@ -0,0 +1,23 @@
+package org.kayteam.actionapi.expansions;
+
+import org.kayteam.actionapi.Action;
+import org.kayteam.actionapi.ActionExpansion;
+import org.kayteam.actionapi.actions.ConsoleAction;
+import org.kayteam.actionapi.util.ActionUtil;
+
+public class ConsoleExpansion extends ActionExpansion {
+
+    public ConsoleExpansion() {
+        super( "console" );
+    }
+
+    @Override
+    public Action generateAction( String format ) {
+
+        String value = ActionUtil.getValue( format );
+
+        return new ConsoleAction( value );
+
+    }
+
+}
