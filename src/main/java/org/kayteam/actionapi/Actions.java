196b2cbc099e2fd59b177bd6c159b0e6b215f671
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-06 22:37:10 -0300

    Actions created.

---
+++ src/main/java/org/kayteam/actionapi/Actions.java
@@ -0,0 +1,47 @@
+package org.kayteam.actionapi;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class Actions {
+
+    private final LinkedHashMap< String , Action > actions = new LinkedHashMap<>();
+
+    public LinkedHashMap< String , Action > getActions() {
+        return actions;
+    }
+
+    public boolean existAction( String format ) {
+        return actions.containsKey( format );
+    }
+
+    public void addAction( Action action ) {
+        actions.put( action.getType() , action );
+    }
+
+    public Action getAction( String format ) {
+        return actions.get( format );
+    }
+
+    public void removeAction( String format ) {
+        actions.remove( format );
+    }
+
+    public void executeAll( Player player ) {
+        actions.values().forEach( action -> action.execute( player ) );
+    }
+
+    public List<String> serialize() {
+
+        List<String> result = new ArrayList<>();
+
+        for ( Action action : actions.values() ) result.add( action.toString() );
+
+        return result;
+
+    }
+
+}
