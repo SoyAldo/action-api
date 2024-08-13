9ef5ac51b9933aa6da5f1de3ff8be774709f9f05
SoyAldo <pereyraosvaldo@live.com.ar>
2024-08-13 17:38:19 -0300

    update

--- src/main/java/com/soyaldo/actionapi/Actions.java
+++ src/main/java/com/soyaldo/actionapi/Actions.java
@@ -42,4 +42,33 @@ public class Actions {
         for (Action action : actions) action.execute(player, requirements);
     }
 
+    public void asd() {
+        String action = "(message) Hi :D <delay=10> <async>";
+
+        String type = "";
+        String content = "";
+        List<String> extras = new ArrayList<>();
+
+        String[] parts = action.split(" ");
+        for (String part : parts) {
+            if (part.startsWith("(") && part.endsWith(")")) {
+                type = part.substring(1, part.length() - 1);
+                continue;
+            }
+            if (part.startsWith("<") && part.endsWith(">")) {
+                extras.add(part.substring(1, part.length() - 1));
+                continue;
+            }
+
+            content = content + part + " ";
+        }
+        if (!content.isEmpty()) {
+            content = content.substring(0, content.length() - 1);
+        }
+
+        System.out.println("Type: '" + type + "'");
+        System.out.println("Content: '" + content + "'");
+        System.out.println("Extras: '" + extras.toString() + "'");
+    }
+
 }
