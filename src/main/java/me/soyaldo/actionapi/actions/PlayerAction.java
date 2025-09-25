abeb7bd29329422362c63073a17c8a947da69673
SoyAldo <pereyraosvaldo@live.com.ar>
2025-09-24 21:08:39 -0300

    [update] The "player" action has optimized

--- src/main/java/me/soyaldo/actionapi/actions/PlayerAction.java
+++ src/main/java/me/soyaldo/actionapi/actions/PlayerAction.java
@@ -1,10 +1,13 @@
 package me.soyaldo.actionapi.actions;
 
 import me.soyaldo.actionapi.models.Action;
-import me.soyaldo.actionapi.util.ActionInfo;
-import me.soyaldo.actionapi.util.MineDown;
-import me.soyaldo.actionapi.util.PlaceholderApi;
+import me.soyaldo.actionapi.models.ActionInfo;
+import me.soyaldo.actionapi.util.ChatUtil;
+import me.soyaldo.actionapi.util.PapiUtil;
+import me.soyaldo.actionapi.util.SchedulerUtil;
+import me.soyaldo.actionapi.util.TextUtil;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerAction extends Action {
 
@@ -19,21 +22,16 @@ public class PlayerAction extends Action {
     @Override
     public void executeAction(Player player, String[][] replacements) {
         String command = getContent();
-        // Replacements
-        for (String[] replacement : replacements) {
-            command = command.replace(replacement[0], replacement[1]);
-        }
-        // PlaceholderAPI
-        command = PlaceholderApi.setPlaceholders(player, command);
-        // Color
-        command = MineDown.parseLegacy(command);
+        // Apply replacements
+        command = TextUtil.replace(command, replacements);
+        // Apply PlaceholderAPI
+        command = PapiUtil.setPlaceholders(player, command);
+        // Apply color
+        command = ChatUtil.colorizeLegacy(command);
         // Execute command
+        JavaPlugin plugin = getActionManager().getJavaPlugin();
         String finalCommand = command;
-        getActionManager().getJavaPlugin().getServer().getScheduler().runTaskLater(
-                getActionManager().getJavaPlugin(),
-                () -> getActionManager().getJavaPlugin().getServer().dispatchCommand(player, finalCommand),
-                0L
-        );
+        SchedulerUtil.runTaskLaterSync(plugin, () -> plugin.getServer().dispatchCommand(player, finalCommand));
     }
 
 }
