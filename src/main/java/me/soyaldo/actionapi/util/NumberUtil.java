649f06963f7097f7e0ba9db71854c038a5202c20
SoyAldo <pereyraosvaldo@live.com.ar>
2025-09-25 11:08:35 -0300

    [update] New methods in NumberUtil.java

--- src/main/java/me/soyaldo/actionapi/util/NumberUtil.java
+++ src/main/java/me/soyaldo/actionapi/util/NumberUtil.java
@@ -33,4 +33,66 @@ public class NumberUtil {
         }
     }
 
+    /**
+     * Get a number from specific text.
+     *
+     * @param text The string number.
+     * @param def  The default value if the text not is a text.
+     * @param min  The minimum value
+     * @return The text parsed or default value.
+     */
+    public static double getDouble(String text, double def, double min) {
+        try {
+            return Math.max(Double.parseDouble(text), min);
+        } catch (NumberFormatException ignore) {
+            return def;
+        }
+    }
+
+    /**
+     * Get a number from specific text.
+     *
+     * @param text The string number.
+     * @param def  The default value if the text not is a text.
+     * @return The text parsed or default value.
+     */
+    public static double getDouble(String text, double def) {
+        try {
+            return Double.parseDouble(text);
+        } catch (NumberFormatException ignore) {
+            return def;
+        }
+    }
+
+    /**
+     * Get a number from specific text.
+     *
+     * @param text The string number.
+     * @param def  The default value if the text not is a text.
+     * @param min  The minimum value
+     * @return The text parsed or default value.
+     */
+    public static float getDouble(String text, float def, float min) {
+        try {
+            return Math.max(Float.parseFloat(text), min);
+        } catch (NumberFormatException ignore) {
+            return def;
+        }
+    }
+
+    /**
+     * Get a number from specific text.
+     *
+     * @param text The string number.
+     * @param def  The default value if the text not is a text.
+     * @return The text parsed or default value.
+     */
+    public static float getFloat(String text, float def) {
+        try {
+            return Float.parseFloat(text);
+        } catch (NumberFormatException ignore) {
+            return def;
+        }
+    }
+
 }
